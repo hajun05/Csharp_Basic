f9fac7fc164d1056bd81cb244de054fbdce7df60
hajun05 <hajun0325175@gmail.com>
2024-12-16 13:32:08 +0900

    실습. 변수 및 캐스팅

--- Csharp_Basic/Form1.cs
+++ Csharp_Basic/Form1.cs
@@ -16,8 +16,42 @@ namespace Csharp_Basic
         {
             InitializeComponent();
 
-            textBox_print.Text = "어떤 내용이 출력될까?";
+            byte locket_engine_num = 4;
+            short locket_try_num = 3;
+            int locket_boarding_people = 3;
+            float locket_feul_mount = 100000.5f;
+            double locket_power = 300000.582;
+            decimal locket_spending_money = 54294553.299845m;
 
+            textBox_print.Text
+                += (locket_engine_num.GetType() +
+                " locket_engine_num : " +
+                locket_engine_num.ToString() + "\r\n");
+
+            textBox_print.Text
+                += (locket_try_num.GetType() +
+                " locket_try_num : " +
+                locket_try_num.ToString() + "\r\n");
+
+            textBox_print.Text
+                += (locket_boarding_people.GetType() +
+                " locket_boarding_people : " +
+                locket_boarding_people.ToString() + "\r\n");
+
+            textBox_print.Text
+                += (locket_feul_mount.GetType() +
+                " locket_feul_mount : " +
+                locket_feul_mount.ToString() + "\r\n");
+
+            textBox_print.Text
+                += (locket_power.GetType() +
+                " locket_power : " +
+                locket_power.ToString() + "\r\n");
+
+            textBox_print.Text
+                += (locket_spending_money.GetType() +
+                " locket_spending_money : " +
+                locket_spending_money.ToString() + "\r\n");
         }
     }
 }

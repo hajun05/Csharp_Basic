cfb835e8920fb5e75157fc42450d9df56a7e03be
hajun05 <hajun0325175@gmail.com>
2024-12-19 12:44:29 +0900

    실습. switch문

---
+++ Csharp_Basic/Form3.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Csharp_Basic
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTxt = new System.Windows.Forms.TextBox();
+            this.InputButton = new System.Windows.Forms.Button();
+            this.Monday = new System.Windows.Forms.RadioButton();
+            this.Tuesday = new System.Windows.Forms.RadioButton();
+            this.Wednesday = new System.Windows.Forms.RadioButton();
+            this.Thursday = new System.Windows.Forms.RadioButton();
+            this.Friday = new System.Windows.Forms.RadioButton();
+            this.Saturday = new System.Windows.Forms.RadioButton();
+            this.Sunday = new System.Windows.Forms.RadioButton();
+            this.OutputTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTxt
+            // 
+            this.InputTxt.Location = new System.Drawing.Point(13, 13);
+            this.InputTxt.Name = "InputTxt";
+            this.InputTxt.Size = new System.Drawing.Size(255, 25);
+            this.InputTxt.TabIndex = 0;
+            // 
+            // InputButton
+            // 
+            this.InputButton.Location = new System.Drawing.Point(274, 12);
+            this.InputButton.Name = "InputButton";
+            this.InputButton.Size = new System.Drawing.Size(75, 27);
+            this.InputButton.TabIndex = 1;
+            this.InputButton.Text = "입력";
+            this.InputButton.UseVisualStyleBackColor = true;
+            this.InputButton.Click += new System.EventHandler(this.InputButton_Click);
+            // 
+            // Monday
+            // 
+            this.Monday.AutoSize = true;
+            this.Monday.Location = new System.Drawing.Point(13, 45);
+            this.Monday.Name = "Monday";
+            this.Monday.Size = new System.Drawing.Size(43, 19);
+            this.Monday.TabIndex = 2;
+            this.Monday.TabStop = true;
+            this.Monday.Text = "월";
+            this.Monday.UseVisualStyleBackColor = true;
+            this.Monday.CheckedChanged += new System.EventHandler(this.Monday_CheckedChanged);
+            // 
+            // Tuesday
+            // 
+            this.Tuesday.AutoSize = true;
+            this.Tuesday.Location = new System.Drawing.Point(62, 45);
+            this.Tuesday.Name = "Tuesday";
+            this.Tuesday.Size = new System.Drawing.Size(43, 19);
+            this.Tuesday.TabIndex = 3;
+            this.Tuesday.TabStop = true;
+            this.Tuesday.Text = "화";
+            this.Tuesday.UseVisualStyleBackColor = true;
+            this.Tuesday.CheckedChanged += new System.EventHandler(this.Tuesday_CheckedChanged);
+            // 
+            // Wednesday
+            // 
+            this.Wednesday.AutoSize = true;
+            this.Wednesday.Location = new System.Drawing.Point(111, 45);
+            this.Wednesday.Name = "Wednesday";
+            this.Wednesday.Size = new System.Drawing.Size(43, 19);
+            this.Wednesday.TabIndex = 4;
+            this.Wednesday.TabStop = true;
+            this.Wednesday.Text = "수";
+            this.Wednesday.UseVisualStyleBackColor = true;
+            this.Wednesday.CheckedChanged += new System.EventHandler(this.Wednesday_CheckedChanged);
+            // 
+            // Thursday
+            // 
+            this.Thursday.AutoSize = true;
+            this.Thursday.Location = new System.Drawing.Point(160, 45);
+            this.Thursday.Name = "Thursday";
+            this.Thursday.Size = new System.Drawing.Size(43, 19);
+            this.Thursday.TabIndex = 5;
+            this.Thursday.TabStop = true;
+            this.Thursday.Text = "목";
+            this.Thursday.UseVisualStyleBackColor = true;
+            this.Thursday.CheckedChanged += new System.EventHandler(this.Thursday_CheckedChanged);
+            // 
+            // Friday
+            // 
+            this.Friday.AutoSize = true;
+            this.Friday.Location = new System.Drawing.Point(209, 45);
+            this.Friday.Name = "Friday";
+            this.Friday.Size = new System.Drawing.Size(43, 19);
+            this.Friday.TabIndex = 6;
+            this.Friday.TabStop = true;
+            this.Friday.Text = "금";
+            this.Friday.UseVisualStyleBackColor = true;
+            this.Friday.CheckedChanged += new System.EventHandler(this.Friday_CheckedChanged);
+            // 
+            // Saturday
+            // 
+            this.Saturday.AutoSize = true;
+            this.Saturday.Location = new System.Drawing.Point(258, 45);
+            this.Saturday.Name = "Saturday";
+            this.Saturday.Size = new System.Drawing.Size(43, 19);
+            this.Saturday.TabIndex = 7;
+            this.Saturday.TabStop = true;
+            this.Saturday.Text = "토";
+            this.Saturday.UseVisualStyleBackColor = true;
+            this.Saturday.CheckedChanged += new System.EventHandler(this.Saturday_CheckedChanged);
+            // 
+            // Sunday
+            // 
+            this.Sunday.AutoSize = true;
+            this.Sunday.Location = new System.Drawing.Point(307, 45);
+            this.Sunday.Name = "Sunday";
+            this.Sunday.Size = new System.Drawing.Size(43, 19);
+            this.Sunday.TabIndex = 8;
+            this.Sunday.TabStop = true;
+            this.Sunday.Text = "일";
+            this.Sunday.UseVisualStyleBackColor = true;
+            this.Sunday.CheckedChanged += new System.EventHandler(this.Sunday_CheckedChanged);
+            // 
+            // OutputTxt
+            // 
+            this.OutputTxt.Location = new System.Drawing.Point(13, 70);
+            this.OutputTxt.Multiline = true;
+            this.OutputTxt.Name = "OutputTxt";
+            this.OutputTxt.Size = new System.Drawing.Size(337, 232);
+            this.OutputTxt.TabIndex = 9;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 312);
+            this.Controls.Add(this.OutputTxt);
+            this.Controls.Add(this.Sunday);
+            this.Controls.Add(this.Saturday);
+            this.Controls.Add(this.Friday);
+            this.Controls.Add(this.Thursday);
+            this.Controls.Add(this.Wednesday);
+            this.Controls.Add(this.Tuesday);
+            this.Controls.Add(this.Monday);
+            this.Controls.Add(this.InputButton);
+            this.Controls.Add(this.InputTxt);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTxt;
+        private System.Windows.Forms.Button InputButton;
+        private System.Windows.Forms.RadioButton Monday;
+        private System.Windows.Forms.RadioButton Tuesday;
+        private System.Windows.Forms.RadioButton Wednesday;
+        private System.Windows.Forms.RadioButton Thursday;
+        private System.Windows.Forms.RadioButton Friday;
+        private System.Windows.Forms.RadioButton Saturday;
+        private System.Windows.Forms.RadioButton Sunday;
+        private System.Windows.Forms.TextBox OutputTxt;
+    }
+}
---
+++ Csharp_Basic/Form3.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_Basic
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private enum week_name
+        {
+            monday = 0,
+            tuesday, 
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday,
+            not = -1
+        }
+
+        private void InputButton_Click(object sender, EventArgs e)
+        {
+            if (InputTxt.Text.Length > 0 )
+            {
+                string input_str = InputTxt.Text;
+                week_name choice_day = new week_name();
+
+                switch (input_str)
+                {
+                    case "monday": case "mon": case "월요일": case "월":
+                        choice_day = week_name.monday;
+                        break;
+                    case "tuesday": case "tues": case "화요일": case "화":
+                        choice_day = week_name.tuesday;
+                        break;
+                    case "wednesday": case "wednes": case "수요일": case "수":
+                        choice_day = week_name.wednesday;
+                        break;
+                    case "thursday": case "thurs": case "목요일": case "목":
+                        choice_day = week_name.thursday;
+                        break;
+                    case "friday": case "fri": case "금요일": case "금":
+                        choice_day = week_name.friday;
+                        break;
+                    case "saturday": case "satur": case "토요일": case "토":
+                        choice_day = week_name.saturday;
+                        break;
+                    case "sunday": case "sun": case "일요일": case "일":
+                        choice_day = week_name.sunday;
+                        break;
+                    default:
+                        choice_day = week_name.not;
+                        break;
+                }
+                DayOutput(choice_day);
+            }
+        }
+
+        private void Monday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "monday";
+            InputButton.PerformClick();
+        }
+        private void Tuesday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "tuesday";
+            InputButton.PerformClick();
+        }
+        private void Wednesday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "wednesday";
+            InputButton.PerformClick();
+        }
+        private void Thursday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "thursday";
+            InputButton.PerformClick();
+        }
+        private void Friday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "friday";
+            InputButton.PerformClick();
+        }
+        private void Saturday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "saturday";
+            InputButton.PerformClick();
+        }
+        private void Sunday_CheckedChanged(object sender, EventArgs e)
+        {
+            InputTxt.Text = "sunday";
+            InputButton.PerformClick();
+        }
+
+        private void DayOutput(week_name choice_day)
+        {
+            switch (choice_day)
+            {
+                case week_name.monday:
+                    OutputTxt.Text = "이야 월요일이다! 출근한다!";
+                    break;
+                case week_name.tuesday:
+                    OutputTxt.Text = "벌써 주말이 그리운 화요일...";
+                    break;
+                case week_name.wednesday:
+                    OutputTxt.Text = "이제 절반 넘긴 수요일!";
+                    break;
+                case week_name.thursday:
+                    OutputTxt.Text = "2일 남았다! 힘내는 목요일!";
+                    break;
+                case week_name.friday:
+                    OutputTxt.Text = "불타는 금요일! 어차피 내일은 주말!";
+                    break;
+                case week_name.saturday:
+                    OutputTxt.Text = "주말의 시작인 토요일!";
+                    break;
+                case week_name.sunday:
+                    OutputTxt.Text = "벌써 일요일이라니, 내일 월요일이라니...";
+                    break;
+            }
+        }
+    }
+}

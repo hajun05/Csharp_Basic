35c4b5d1a15877af824f331ef99c6a0b3dc7de99
hajun05 <hajun0325175@gmail.com>
2024-12-20 12:46:23 +0900

    실습. while문(실수로 while문 불필요한 문제 제출)

---
+++ Csharp_Basic/Form5.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Csharp_Basic
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScissorsButton = new System.Windows.Forms.Button();
+            this.RockButton = new System.Windows.Forms.Button();
+            this.PaperButton = new System.Windows.Forms.Button();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ScissorsButton
+            // 
+            this.ScissorsButton.Location = new System.Drawing.Point(13, 13);
+            this.ScissorsButton.Name = "ScissorsButton";
+            this.ScissorsButton.Size = new System.Drawing.Size(75, 23);
+            this.ScissorsButton.TabIndex = 0;
+            this.ScissorsButton.Text = "가위";
+            this.ScissorsButton.UseVisualStyleBackColor = true;
+            this.ScissorsButton.Click += new System.EventHandler(this.ScissorsButton_Click);
+            // 
+            // RockButton
+            // 
+            this.RockButton.Location = new System.Drawing.Point(95, 13);
+            this.RockButton.Name = "RockButton";
+            this.RockButton.Size = new System.Drawing.Size(75, 23);
+            this.RockButton.TabIndex = 1;
+            this.RockButton.Text = "바위";
+            this.RockButton.UseVisualStyleBackColor = true;
+            this.RockButton.Click += new System.EventHandler(this.RockButton_Click);
+            // 
+            // PaperButton
+            // 
+            this.PaperButton.Location = new System.Drawing.Point(176, 13);
+            this.PaperButton.Name = "PaperButton";
+            this.PaperButton.Size = new System.Drawing.Size(75, 23);
+            this.PaperButton.TabIndex = 2;
+            this.PaperButton.Text = "보";
+            this.PaperButton.UseVisualStyleBackColor = true;
+            this.PaperButton.Click += new System.EventHandler(this.PaperButton_Click);
+            // 
+            // OutputText
+            // 
+            this.OutputText.Location = new System.Drawing.Point(13, 43);
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(238, 184);
+            this.OutputText.TabIndex = 3;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 239);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.PaperButton);
+            this.Controls.Add(this.RockButton);
+            this.Controls.Add(this.ScissorsButton);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ScissorsButton;
+        private System.Windows.Forms.Button RockButton;
+        private System.Windows.Forms.Button PaperButton;
+        private System.Windows.Forms.TextBox OutputText;
+    }
+}
---
+++ Csharp_Basic/Form5.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_Basic
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        enum RockScissorPaper
+        {
+            Rock,
+            Scissor,
+            Paper
+        }
+        byte my_score = 0;
+        byte com_score = 0;
+
+        private void ScissorsButton_Click(object sender, EventArgs e)
+        {
+            RockScissorPaper my_choice = RockScissorPaper.Scissor;
+            MeVSComputer(my_choice);
+        }
+
+        private void RockButton_Click(object sender, EventArgs e)
+        {
+            RockScissorPaper my_choice = RockScissorPaper.Rock;
+            MeVSComputer(my_choice);
+        }
+
+        private void PaperButton_Click(object sender, EventArgs e)
+        {
+            RockScissorPaper my_choice = RockScissorPaper.Paper;
+            MeVSComputer(my_choice);
+        }
+
+        private void MeVSComputer(RockScissorPaper my_choice)
+        {
+            Random random = new Random();
+            int com_choice_num = random.Next(0, 3);
+            RockScissorPaper com_choice = new RockScissorPaper();
+
+            switch (com_choice_num)
+            {
+                case (int)RockScissorPaper.Rock:
+                    com_choice = RockScissorPaper.Rock;
+                    break;
+                case (int)RockScissorPaper.Scissor:
+                    com_choice = RockScissorPaper.Scissor;
+                    break;
+                case (int)RockScissorPaper.Paper:
+                    com_choice = RockScissorPaper.Paper;
+                    break;
+            }
+
+            GameResultOutput(my_choice, com_choice);
+        }
+
+        private void GameResultOutput(RockScissorPaper my_choice, RockScissorPaper com_choice)
+        {
+            OutputText.Text = "";
+            OutputText.Text += string.Format($"내 선택 : {my_choice.ToString()}\r\nVS\r\n컴퓨터의 선택 : {com_choice.ToString()}\r\n");
+
+            if (my_choice == com_choice)
+            {
+                OutputText.Text += "비김!\r\n";
+            }
+            else
+            {
+                if (my_choice == RockScissorPaper.Rock && com_choice == RockScissorPaper.Scissor ||
+                    my_choice == RockScissorPaper.Scissor && com_choice == RockScissorPaper.Paper ||
+                    my_choice == RockScissorPaper.Paper && com_choice == RockScissorPaper.Rock)
+                {
+                    OutputText.Text += "나의 승리!\r\n";
+                    my_score += 1;
+                }
+                else
+                {
+                    OutputText.Text += "나의 패배...\r\n";
+                    com_score += 1;
+                }
+            }
+
+            OutputText.Text += string.Format("내 점수 : {0} - 컴퓨터의 점수 : {1}\r\n", my_score, com_score);
+            if (my_score >= 3)
+            {
+                OutputText.Text += "나의 최종 승리!(점수 초기화)\r\n";
+                my_score = 0;
+                com_score = 0;
+            }
+            else if (com_score >= 3)
+            {
+                OutputText.Text += "나의 최종 패배...(점수 초기화)\r\n";
+                my_score = 0;
+                com_score = 0;
+            }
+        }
+    }
+}

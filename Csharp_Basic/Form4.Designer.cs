ec64d376ee19bcbd328230014071e42f20b09bf4
hajun05 <hajun0325175@gmail.com>
2024-12-20 10:24:45 +0900

    실습. for문 v1

---
+++ Csharp_Basic/Form4.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Csharp_Basic
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputText = new System.Windows.Forms.TextBox();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.InputButton = new System.Windows.Forms.Button();
+            this.InstructionText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputText
+            // 
+            this.InputText.Location = new System.Drawing.Point(13, 34);
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(152, 25);
+            this.InputText.TabIndex = 0;
+            // 
+            // OutputText
+            // 
+            this.OutputText.Location = new System.Drawing.Point(13, 65);
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.Size = new System.Drawing.Size(233, 328);
+            this.OutputText.TabIndex = 1;
+            // 
+            // InputButton
+            // 
+            this.InputButton.Font = new System.Drawing.Font("Gulim", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.InputButton.Location = new System.Drawing.Point(171, 31);
+            this.InputButton.Name = "InputButton";
+            this.InputButton.Size = new System.Drawing.Size(75, 25);
+            this.InputButton.TabIndex = 2;
+            this.InputButton.Text = "input";
+            this.InputButton.UseVisualStyleBackColor = true;
+            this.InputButton.Click += new System.EventHandler(this.InputTextButtonClick);
+            // 
+            // InstructionText
+            // 
+            this.InstructionText.BackColor = System.Drawing.SystemColors.Control;
+            this.InstructionText.Location = new System.Drawing.Point(13, 3);
+            this.InstructionText.Name = "InstructionText";
+            this.InstructionText.Size = new System.Drawing.Size(233, 25);
+            this.InstructionText.TabIndex = 3;
+            this.InstructionText.Text = "학생 수를 입력하십시오.";
+            this.InstructionText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 450);
+            this.Controls.Add(this.InstructionText);
+            this.Controls.Add(this.InputButton);
+            this.Controls.Add(this.OutputText);
+            this.Controls.Add(this.InputText);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputText;
+        private System.Windows.Forms.TextBox OutputText;
+        private System.Windows.Forms.Button InputButton;
+        private System.Windows.Forms.TextBox InstructionText;
+    }
+}
---
+++ Csharp_Basic/Form4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_Basic
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextButtonClick(object sender, EventArgs e)
+        {
+            int students_num = 0;
+            if (int.TryParse(InputText.Text, out students_num))
+            {
+                int[] students_score = new int[students_num];
+                Random score = new Random();
+                for(int i = 0; i < students_num; i++)
+                {
+                    students_score[i] = score.Next(0, 101);
+                }
+                OutputStudentsScore(students_num, students_score);
+            }
+            else
+            {
+                OutputText.Text = "잘못된 값을 입력했습니다.";
+            }
+        }
+
+        private void OutputStudentsScore(int student_num, int[] students_score)
+        {
+            OutputText.Text = "";
+            for (int i = 0; i < student_num; i++)
+            {
+                OutputText.Text += string.Format("학생{0}의 점수 : {1}점.\r\n", i, students_score[i]);
+            }
+        }
+    }
+}

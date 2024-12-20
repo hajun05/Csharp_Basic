5621cd9af2d6b7baa3c1f40e832abc12dd712022
hajun05 <hajun0325175@gmail.com>
2024-12-20 12:02:46 +0900

    실습. for문 v2 (추가문제) 1. 성적을 국어, 영어, 수학 3가지로 늘이기 2. 2차원 배열로 만들어서 데이터를 저장하기 3. 2차원 배열을 DataGridView 컨트롤에 표현하기 4. OpenFileDialog를 이용하여 2차원 배열에 있는 데이터를 csv 파일로 저장하기 -> 4. 김예린 크루의 repo 참고. https://github.com/Kim-yerin0904/example_241218/blob/main/example-241220/Form1.cs

--- Csharp_Basic/Form4.Designer.cs
+++ Csharp_Basic/Form4.Designer.cs
@@ -29,34 +29,33 @@
         private void InitializeComponent()
         {
             this.InputText = new System.Windows.Forms.TextBox();
-            this.OutputText = new System.Windows.Forms.TextBox();
             this.InputButton = new System.Windows.Forms.Button();
             this.InstructionText = new System.Windows.Forms.TextBox();
+            this.StudentsScoreGridView = new System.Windows.Forms.DataGridView();
+            this.Student = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Korean = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Math = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.English = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentsNum = new System.Windows.Forms.Label();
+            this.ExportButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsScoreGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // InputText
             // 
-            this.InputText.Location = new System.Drawing.Point(13, 34);
+            this.InputText.Location = new System.Drawing.Point(76, 34);
             this.InputText.Name = "InputText";
-            this.InputText.Size = new System.Drawing.Size(152, 25);
+            this.InputText.Size = new System.Drawing.Size(103, 25);
             this.InputText.TabIndex = 0;
-            // 
-            // OutputText
-            // 
-            this.OutputText.Location = new System.Drawing.Point(13, 65);
-            this.OutputText.Multiline = true;
-            this.OutputText.Name = "OutputText";
-            this.OutputText.Size = new System.Drawing.Size(233, 328);
-            this.OutputText.TabIndex = 1;
             // 
             // InputButton
             // 
             this.InputButton.Font = new System.Drawing.Font("Gulim", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.InputButton.Location = new System.Drawing.Point(171, 31);
+            this.InputButton.Location = new System.Drawing.Point(185, 34);
             this.InputButton.Name = "InputButton";
-            this.InputButton.Size = new System.Drawing.Size(75, 25);
+            this.InputButton.Size = new System.Drawing.Size(84, 26);
             this.InputButton.TabIndex = 2;
-            this.InputButton.Text = "input";
+            this.InputButton.Text = "Input";
             this.InputButton.UseVisualStyleBackColor = true;
             this.InputButton.Click += new System.EventHandler(this.InputTextButtonClick);
             // 
@@ -65,22 +64,90 @@
             this.InstructionText.BackColor = System.Drawing.SystemColors.Control;
             this.InstructionText.Location = new System.Drawing.Point(13, 3);
             this.InstructionText.Name = "InstructionText";
-            this.InstructionText.Size = new System.Drawing.Size(233, 25);
+            this.InstructionText.Size = new System.Drawing.Size(358, 25);
             this.InstructionText.TabIndex = 3;
             this.InstructionText.Text = "학생 수를 입력하십시오.";
             this.InstructionText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // StudentsScoreGridView
+            // 
+            this.StudentsScoreGridView.AllowUserToAddRows = false;
+            this.StudentsScoreGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.StudentsScoreGridView.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.StudentsScoreGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentsScoreGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Student,
+            this.Korean,
+            this.Math,
+            this.English});
+            this.StudentsScoreGridView.Location = new System.Drawing.Point(13, 66);
+            this.StudentsScoreGridView.Name = "StudentsScoreGridView";
+            this.StudentsScoreGridView.RowHeadersVisible = false;
+            this.StudentsScoreGridView.RowHeadersWidth = 51;
+            this.StudentsScoreGridView.RowTemplate.Height = 27;
+            this.StudentsScoreGridView.Size = new System.Drawing.Size(358, 372);
+            this.StudentsScoreGridView.TabIndex = 4;
+            // 
+            // Student
+            // 
+            this.Student.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Student.HeaderText = "학생";
+            this.Student.MinimumWidth = 6;
+            this.Student.Name = "Student";
+            this.Student.Width = 66;
+            // 
+            // Korean
+            // 
+            this.Korean.HeaderText = "국어";
+            this.Korean.MinimumWidth = 6;
+            this.Korean.Name = "Korean";
+            // 
+            // Math
+            // 
+            this.Math.HeaderText = "수학";
+            this.Math.MinimumWidth = 6;
+            this.Math.Name = "Math";
+            // 
+            // English
+            // 
+            this.English.HeaderText = "영어";
+            this.English.MinimumWidth = 6;
+            this.English.Name = "English";
+            // 
+            // StudentsNum
+            // 
+            this.StudentsNum.Location = new System.Drawing.Point(13, 37);
+            this.StudentsNum.Name = "StudentsNum";
+            this.StudentsNum.Size = new System.Drawing.Size(57, 22);
+            this.StudentsNum.TabIndex = 5;
+            this.StudentsNum.Text = "학생 수";
+            this.StudentsNum.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Font = new System.Drawing.Font("Gulim", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ExportButton.Location = new System.Drawing.Point(275, 33);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(96, 26);
+            this.ExportButton.TabIndex = 6;
+            this.ExportButton.Text = "Export";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButtonClick);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 450);
+            this.ClientSize = new System.Drawing.Size(383, 450);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.StudentsNum);
+            this.Controls.Add(this.StudentsScoreGridView);
             this.Controls.Add(this.InstructionText);
             this.Controls.Add(this.InputButton);
-            this.Controls.Add(this.OutputText);
             this.Controls.Add(this.InputText);
             this.Name = "Form4";
             this.Text = "Form4";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsScoreGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,8 +156,14 @@
         #endregion
 
         private System.Windows.Forms.TextBox InputText;
-        private System.Windows.Forms.TextBox OutputText;
         private System.Windows.Forms.Button InputButton;
         private System.Windows.Forms.TextBox InstructionText;
+        private System.Windows.Forms.DataGridView StudentsScoreGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Student;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Korean;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Math;
+        private System.Windows.Forms.DataGridViewTextBoxColumn English;
+        private System.Windows.Forms.Label StudentsNum;
+        private System.Windows.Forms.Button ExportButton;
     }
 }
--- Csharp_Basic/Form4.cs
+++ Csharp_Basic/Form4.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ using System.Windows.Forms;
 
 namespace Csharp_Basic
 {
+    //성적을 국어, 영어, 수학 3가지로 늘이기
+    //2차원 배열로 만들어서 데이터를 저장하기
+    //2차원 배열을 DataGridView 컨트롤에 표현하기
+    //OpenFileDialog를 이용하여 2차원 배열에 있는 데이터를 csv 파일로 저장하기
+
     public partial class Form4 : Form
     {
         public Form4()
@@ -17,32 +23,119 @@ namespace Csharp_Basic
             InitializeComponent();
         }
 
-        private void InputTextButtonClick(object sender, EventArgs e)
+        enum Subjects // 열거형으로 과목별 점수 접근(직관성)
+        {
+            Korean,
+            Math,
+            English
+        }
+
+        private void InputTextButtonClick(object sender, EventArgs e) // 학생수 입력 함수
         {
             int students_num = 0;
             if (int.TryParse(InputText.Text, out students_num))
             {
-                int[] students_score = new int[students_num];
-                Random score = new Random();
-                for(int i = 0; i < students_num; i++)
-                {
-                    students_score[i] = score.Next(0, 101);
-                }
+                InstructionText.Text = "학생 수를 입력하십시오."; // 지시문 초기화
+                int[,] students_score = StudentScoreGeneration(students_num);
                 OutputStudentsScore(students_num, students_score);
             }
             else
             {
-                OutputText.Text = "잘못된 값을 입력했습니다.";
+                InstructionText.Text = "잘못된 값을 입력했습니다. 다시 입력하십시오."; // 숫자 이외의 값 입력시 재 입력 지시
             }
         }
 
-        private void OutputStudentsScore(int student_num, int[] students_score)
+        private void OutputStudentsScore(int student_num, int[,] students_score) // 학생들 점수 출력 함수
         {
-            OutputText.Text = "";
-            for (int i = 0; i < student_num; i++)
+            StudentsScoreGridView.Rows.Clear(); // 기존의 datagrid 내용 제거
+
+            for (int i = 0; i < student_num; i++) // 새로운 datagrid 내용 추가
             {
-                OutputText.Text += string.Format("학생{0}의 점수 : {1}점.\r\n", i, students_score[i]);
+                StudentsScoreGridView.Rows.Add("학생" + i.ToString(),
+                    students_score[i, (int)Subjects.Korean], // 열거형을 이용한 과목 지정
+                    students_score[i, (int)Subjects.Math], 
+                    students_score[i, (int)Subjects.English]);
             }
+        }
+
+        private int[,] StudentScoreGeneration(int students_num) // 2차원 배열로 학생들 점수 저장
+        {
+            int[,] students_score = new int[students_num, 3];
+            Random score = new Random(); 
+            
+            for (int i = 0; i < students_num; i++) // 각 과목별 랜덤 점수 저장
+            {
+                students_score[i, (int)Subjects.Korean] = score.Next(0, 101); // 열거형을 이용한 과목 지정
+                students_score[i, (int)Subjects.Math] = score.Next(0, 101);
+                students_score[i, (int)Subjects.English] = score.Next(0, 101);
+            }
+
+            return students_score; // 점수를 저장한 2차열 배열 반환
+        }
+
+        private void ExportButtonClick(object sender, EventArgs e) // 학생들 점수 cvs파일로 출력. 김예린 크루의 repo 참고.
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV 파일 |*.csv|모든 파일|*.*";
+            saveFileDialog.FilterIndex = 1;
+            // 대화상자를 닫기 전에 디렉토리를 이전에 선택한 디렉토리로 복원한지의 여부를 나타냄
+            saveFileDialog.RestoreDirectory = true;
+            // 확장명을 입려하지 않을 경우, 자동으로 csv로 지정
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.DefaultExt = "csv";
+            // 파일이 이미 존재하면 덮어쓰기를 할지 묻는 대화상자 표시 기본값 = true
+            saveFileDialog.OverwritePrompt = true;
+            // 저장할 위치의 초기 디렉토리 설정
+            saveFileDialog.InitialDirectory = Environment.CurrentDirectory;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DataGridViewToCSV(saveFileDialog.FileName, StudentsScoreGridView, true);
+            }
+        }
+        private void DataGridViewToCSV(string fileName, DataGridView dgview, bool header)
+        {
+            string delimiter = ","; //구분자
+            FileStream fs = new FileStream(fileName, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            StreamWriter csvExport = new StreamWriter(fs, System.Text.Encoding.UTF8);
+
+            if (dgview.Rows.Count == 0) return;
+
+            // header 포함
+            if (header)
+            {
+                for (int i = 0; i < dgview.Columns.Count; i++)
+                {
+                    csvExport.Write(dgview.Columns[i].HeaderText);
+                    if (i != dgview.Columns.Count - 1)
+                    {
+                        csvExport.Write(delimiter);
+                    }
+                }
+            }
+            csvExport.Write(csvExport.NewLine); //add new line
+
+            //데이터 출력
+            foreach (DataGridViewRow row in dgview.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    for (int i = 0; i < dgview.Columns.Count; i++)
+                    {
+                        csvExport.Write(row.Cells[i].Value);
+                        if (i != dgview.Columns.Count - 1)
+                        {
+                            csvExport.Write(delimiter);
+                        }
+                    }
+                    csvExport.Write(csvExport.NewLine); //write new line
+                }
+            }
+            csvExport.Flush();
+            csvExport.Close();
+            fs.Close();
+
+            InstructionText.Text = "CSV 파일 저장 완료!";
         }
     }
 }

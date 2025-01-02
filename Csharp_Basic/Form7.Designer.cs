5be1667aefac96bcdff239c6d3edfc381c1a5484
hajun05 <hajun0325175@gmail.com>
2025-01-02 13:44:57 +0900

    실습. 예외처리 v2

--- Csharp_Basic/Form7.Designer.cs
+++ Csharp_Basic/Form7.Designer.cs
@@ -28,19 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 238);
+            this.textBox1.TabIndex = 0;
             // 
             // Form7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 450);
+            this.ClientSize = new System.Drawing.Size(354, 450);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form7";
             this.Text = "Form7";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Csharp_Basic/Form7.cs
+++ Csharp_Basic/Form7.cs
@@ -13,42 +13,84 @@ namespace Csharp_Basic
 {
     public partial class Form7 : Form
     {
-        string content = File.ReadAllText(@"C:\Users\Hajun\source\repos\Csharp_Basic\input.txt");
         public Form7()
         {
             InitializeComponent();
 
-            FileInput(content, out int[] file_input);
-        }
-
-        public void FileInput(string file_content, out int[] file_input)
-        {
-            string[] content = file_content.Split('\n');
-            file_input = new int[content.Length];
             try
             {
-                for (int i = 0; i < content.Length; i++)
+                string content = File.ReadAllText(@"C:\Users\Hajun\source\repos\Csharp_Basic\input.txt");
+
+                FileInput(content, out List<int> file_input);
+            }
+            catch(DirectoryNotFoundException e)
+            {
+                MessageBox.Show("존재하지 않는 파일 경로입니다.");
+            }
+        }
+
+        public void FileInput(string file_content, out List<int> file_input)
+        {
+            string[] content = file_content.Split('\n'); 
+            file_input = new List<int>();
+            int i = 0;
+            try
+            {
+                for (i = 0; i < content.Length; i++)
                 {
-                    file_input[i] = int.Parse(content[i]);
+                    file_input.Add(int.Parse(content[i]));
                 }
             }
-            catch (ArgumentNullException e)
+            catch (ArgumentNullException)
             {
-                MessageBox.Show("존재하지 않는 파일입니다.");
+                MessageBox.Show("NULL 위치를 참조했습니다.");
             }
-            catch (OverflowException e)
+            catch (OverflowException)
             {
                 MessageBox.Show("int형의 크기로 처리할 수 없는 수입니다.");
             }
-            catch (FormatException e)
+            catch (FormatException)
             {
                 MessageBox.Show("숫자로 변환할 수 없는 파일 내용이 존재합니다.");
+                for (int j = i; j < content.Length; j++)
+                {
+                    int convert;
+                    if (int.TryParse(content[j], out convert)) 
+                    {
+                        file_input.Add(convert);
+                    }
+                    else
+                    {
+                        string temp = "";
+                        foreach (char c in content[j])
+                        {
+                            if ('0' <= c && c <= '9')
+                            {
+                                temp += c;
+                            }
+                            else if (c == '\n')
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+
+                        if (temp != "")
+                        {
+                            int.TryParse(temp, out convert);
+                            file_input.Add(convert);
+                        }
+                    }
+                }
             }
             finally
             {
-                for (int i = 0; i < file_input.Length; i++)
+                for (int n = 0; n < file_input.Count; n++)
                 {
-                    MessageBox.Show(file_input[i].ToString());
+                    textBox1.Text += file_input[n].ToString() + "\r\n";
                 }
             }
         }

681e6f9d51279a2f74cf83795983d9768b519ff8
hajun05 <hajun0325175@gmail.com>
2025-01-03 12:47:25 +0900

    실습. 제네릭 딕셔너리 v2(using문, 예외처리 추가)

--- Csharp_Basic/Form8.cs
+++ Csharp_Basic/Form8.cs
@@ -30,10 +30,11 @@ namespace Csharp_Basic
             try
             {
                 //인스턴스 생성
-                OpenFileDialog ofd = new OpenFileDialog();
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
 
-                // 사용할 필터(확장자) 설정
-                List<string> filterList = new List<string>{
+                    // 사용할 필터(확장자) 설정
+                    List<string> filterList = new List<string>{
                     "Bitmap(*.bmp, *dib)|*.bmp;*.dib;",
                     "|JPEG(*.jpg, *.jpeg, *.jpe, *.jfif)|*.jpg; *.jpeg; *.jpe; *.jfif;",
                     "|TIFF(*.tif, *.tiff)|*.tif; *.tiff;", "|GIF(*.gif)|*.gif",
@@ -41,66 +42,69 @@ namespace Csharp_Basic
                     "|TXT(*.txt)|*.txt;"
                 };
 
-                // 필터(확장자 적용)
-                foreach (string filter in filterList)
-                { ofd.Filter += filter; }
+                    // 필터(확장자 적용)
+                    foreach (string filter in filterList)
+                    { ofd.Filter += filter; }
 
-                // 또는 아래와 같이 한꺼번에 Supported Image File로 표시할 수 있다.
-                //ofd.Filter = "Supported Image File|*jpg;*.jpeg;*.bmp;*.png;*.dib;*.gif;*.tif;";
+                    // 또는 아래와 같이 한꺼번에 Supported Image File로 표시할 수 있다.
+                    //ofd.Filter = "Supported Image File|*jpg;*.jpeg;*.bmp;*.png;*.dib;*.gif;*.tif;";
 
-                ofd.AddExtension = false;                    // 확장자 자동 추가
-                ofd.CheckFileExists = true;                  // 파일 존재하지 않을 경우 메시지 표시 여부
-                ofd.CheckPathExists = true;                  // 경로 존재하지 않을 경우 메시지 표시 여부    
-                ofd.Multiselect = false;                     // 복수 선택
+                    ofd.AddExtension = false;                    // 확장자 자동 추가
+                    ofd.CheckFileExists = true;                  // 파일 존재하지 않을 경우 메시지 표시 여부
+                    ofd.CheckPathExists = true;                  // 경로 존재하지 않을 경우 메시지 표시 여부    
+                    ofd.Multiselect = false;                     // 복수 선택
 
-                ofd.ShowHelp = true;                         // Help 버튼 표시 여부
-                ofd.ShowReadOnly = false;                    // 읽기 전용 체크박스 표시 여부
-                ofd.ReadOnlyChecked = false;                 // 읽기 전용항목 체크 여부
-                ofd.SupportMultiDottedExtensions = true;     // 여러개의 확장자를 표시하는지 여부(저장시 또한)
-                ofd.Title = "Select an image...";            // 타이틀 표시
-                ofd.ValidateNames = true;                    // 파일명의 유효성 검증
+                    ofd.ShowHelp = true;                         // Help 버튼 표시 여부
+                    ofd.ShowReadOnly = false;                    // 읽기 전용 체크박스 표시 여부
+                    ofd.ReadOnlyChecked = false;                 // 읽기 전용항목 체크 여부
+                    ofd.SupportMultiDottedExtensions = true;     // 여러개의 확장자를 표시하는지 여부(저장시 또한)
+                    ofd.Title = "Select an image...";            // 타이틀 표시
+                    ofd.ValidateNames = true;                    // 파일명의 유효성 검증
 
-                //초기 디렉터리 -> 바탕화면으로 설정
-                ofd.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                    //초기 디렉터리 -> 바탕화면으로 설정
+                    ofd.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
 
-                if (ofd.ShowDialog() == DialogResult.OK) // 다이얼로그 표시
-                {
-                    // OpenFileDialog로 접근한 파일을 StreamReader로 열기
-                    StreamReader sr = new StreamReader(ofd.FileName, Encoding.Default);
-
-                    // 파일 내용
-                    //john,12313,01012345678
-                    //raymond,321321,01045671234
-                    //codingon,111111
-                    //posco,2580,026159487
-
-                    while (true)
+                    if (ofd.ShowDialog() == DialogResult.OK) // 다이얼로그 표시
                     {
-                        string line = sr.ReadLine();
-                        if (line != null)
+                        // OpenFileDialog로 접근한 파일을 StreamReader로 열기
+                        using (StreamReader sr = new StreamReader(ofd.FileName, Encoding.Default))
                         {
-                            string[] temp_str = line.Split(',');
-                            id_pw.Add(temp_str[0], temp_str[1]);
-                            if (temp_str.Length > 2)
+                            // 파일 내용
+                            //john,12313,01012345678
+                            //raymond,321321,01045671234
+                            //codingon,111111
+                            //posco,2580,026159487
+
+                            while (true)
                             {
-                                id_phone.Add(temp_str[0], temp_str[2]);
-                            }
-                            else
-                            {
-                                id_phone.Add(temp_str[0], null);
+                                string line = sr.ReadLine();
+                                if (line != null)
+                                {
+                                    string[] temp_str = line.Split(',');
+                                    id_pw.Add(temp_str[0], temp_str[1]);
+                                    if (temp_str.Length > 2)
+                                    {
+                                        id_phone.Add(temp_str[0], temp_str[2]);
+                                    }
+                                    else
+                                    {
+                                        id_phone.Add(temp_str[0], null);
+                                    }
+                                }
+                                else
+                                    break;
                             }
                         }
-                        else
-                            break;
                     }
-                    sr.Close();
                 }
-
-                ofd.Dispose(); // OpenFileDialog 리소스 해제
             }
             catch (IOException ex)
             {
                 MessageBox.Show(ex.ToString(), "존재하지 않거나 다른 위치에서 열려있는 파일입니다.");
+            }
+            catch
+            {
+                MessageBox.Show("유효하지 않은 데이터를 지닌 파일입니다.");
             }
         }
 

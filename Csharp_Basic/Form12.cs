40a057392b0375681713092e8d95671ec9c9d7af
hajun05 <hajun0325175@gmail.com>
2025-01-07 13:49:50 +0900

    실습. async, await v2(OpenFileDialog 사용)

--- Csharp_Basic/Form12.cs
+++ Csharp_Basic/Form12.cs
@@ -29,9 +29,16 @@ namespace Csharp_Basic
         private async void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "";
-            string path = string.Format(@"{0}", textBox2.Text.Replace("\"", ""));
-            string file_content = await ReadFileAsync(path);
-            textBox1.Text = file_content;
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofd.FileName;
+                string file_content = await ReadFileAsync(path);
+                textBox1.Text = file_content;
+            }
+            //string path = string.Format(@"{0}", textBox2.Text.Replace("\"", ""));
+
+            
         }
 
         private async void button2_Click(object sender, EventArgs e)
@@ -41,5 +48,21 @@ namespace Csharp_Basic
             sr = new StreamReader(path);
             string file_content = await sr.ReadToEndAsync();
         }
+
+    //    OpenFileDialog openFileDialog = new OpenFileDialog();
+    //    string fileName = "";
+    //    string result = "";
+
+    //        if (openFileDialog.ShowDialog() == DialogResult.OK)
+    //        {
+    //            fileName = openFileDialog.FileName;
+
+    //            using (StreamReader streamReader = File.OpenText(fileName))
+    //            {
+    //                result = await streamReader.ReadToEndAsync();
+    //}
+
+    //textBox1.Text = result;
+    //        }
     }
 }

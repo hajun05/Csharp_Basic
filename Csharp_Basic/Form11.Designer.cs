55b6161f47b82475ea9cf4eb04dec5981f371993
hajun05 <hajun0325175@gmail.com>
2025-01-07 12:37:12 +0900

    실습. 멀티스레드 레이스

---
+++ Csharp_Basic/Form11.Designer.cs
@@ -0,0 +1,171 @@
+﻿using System.Windows.Forms;
+
+namespace Csharp_Basic
+{
+    partial class Form11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(120, 35);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(120, 64);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(259, 23);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(120, 93);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(259, 23);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(120, 122);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(259, 23);
+            this.progressBar4.TabIndex = 3;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(120, 151);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(259, 23);
+            this.progressBar5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Thread 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Thread 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Thread 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Thread 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Thread 5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 181);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 150);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form11";
+            this.Text = "Form11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+    }
+}
---
+++ Csharp_Basic/Form11.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Csharp_Basic
+{
+    public partial class Form11 : Form
+    {
+        Thread[] thread_racer = new Thread[5];
+        ProgressBar[] progressBars = new ProgressBar[5];
+        int[] race_spand_time = new int[5];
+        Random random = new Random();
+
+        public Form11()
+        {
+            InitializeComponent();
+
+            this.progressBars[0] = progressBar1;
+            this.progressBars[1] = progressBar2;
+            this.progressBars[2] = progressBar3;
+            this.progressBars[3] = progressBar4;
+            this.progressBars[4] = progressBar5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                // 람다식에서의 클로저 문제. 람다식 내부에서는 i 변수의 참조를 캡처, 마지막 루프 당시의 i를 참조하기 때문
+                int racerIndex = i;
+                thread_racer[racerIndex] = new Thread(() => ThreadRace(racerIndex));
+            }
+
+            for(int j = 0; j < 5; j++)
+            {
+                int racerIndex = j;
+                thread_racer[racerIndex].Start();
+            }
+
+            // 메인 스레드를 대기시켜버리며 invoke가 재대로 수행 불가
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    thread_racer[i].Join();
+            //}
+        }
+
+        public void ThreadRace(int racer_num)
+        {
+            //progressBar.Invoke(new Action(() =>
+            //{
+            //    progressBar.Value = nprogress;
+            //    if (nprogress == 100)
+            //    {
+            //        textBox.Invoke(new Action(() => textBox.Text += $"{playerNumber} arrival time : {s[playerNumber]}ms\r\n"));
+            //    }
+            //}));
+
+            while (true)
+            {
+                int race_time_ms = random.Next(100, 1001);
+                race_spand_time[racer_num] += race_time_ms;
+                if (progressBars[racer_num].InvokeRequired)
+                {
+                    if (progressBars[racer_num].Value < 100)
+                    {
+                        Thread.Sleep(race_time_ms);
+                        progressBars[racer_num].Invoke(new MethodInvoker(() =>
+                        {
+                            progressBars[racer_num].Value += 5;
+                        }));
+                    }
+                    else
+                    {
+                        int s = race_spand_time[racer_num] / 1000;
+                        int ms = race_spand_time[racer_num] % 1000;
+                        progressBars[racer_num].Invoke(new MethodInvoker(() =>
+                        {  
+                            textBox1.Text += string.Format($"Thread {racer_num + 1}가 도착했습니다. " +
+                            $"소모 시간 {s}.{ms}초.\r\n");
+                        }));
+                        break;
+                    }
+                }
+                else
+                {
+                    if (progressBars[racer_num].Value < 100)
+                    {
+                        Thread.Sleep(race_time_ms);
+                        progressBars[racer_num].Value += 5;
+                    }
+                    else
+                    {
+                        TimeSpan timeSpan = TimeSpan.FromMilliseconds(race_spand_time[racer_num]);
+                        textBox1.Text += string.Format($"Thread {racer_num + 1}가 도착했습니다. " +
+                            $"소모 시간 {timeSpan.TotalSeconds}.{timeSpan.TotalMilliseconds:D3}초.\r\n");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
